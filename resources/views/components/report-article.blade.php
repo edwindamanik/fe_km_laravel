2507c7c24e708660c40541c4938f5bed2fb384f8
Edwin Immanuel Damanik <edwindamanik5@gmail.com>
2022-09-11 17:59:34 +0700

    update

--- resources/views/components/report-article.blade.php
+++ resources/views/components/report-article.blade.php
@@ -1,6 +1,8 @@
 <div class="report-article-container">
-  <a href="#">
+  <a href="#report">
     <img src="{{ URL::asset('assets/icons/report.svg') }}" alt="report icon" width="16px" height="16px">
     <p>Laporkan Artikel</p>
   </a>
 </div>
+
+@include('components.form-report')
