b2b063121bb6b7a5612087b52ee151244737cc32
Edwin Immanuel Damanik <edwindamanik5@gmail.com>
2022-09-15 11:52:25 +0700

    asd

--- resources/views/components/slider-news.blade.php
+++ resources/views/components/slider-news.blade.php
@@ -1,47 +1,46 @@
-<section class="custom-slider"> 
-  <button class="pre-btn"><img src="{{ URL::asset('assets/icons/prev.svg') }}" alt=""></button>
-  <button class="nxt-btn"><img src="{{ URL::asset('assets/icons/next.svg') }}" alt=""></button>
-  <div class="slider-container">
-      <div class="slider-card">
-          <div class="slider-image">
-              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
-          </div>
-          <div class="slider-info">
-              <span class="slider-title">asd</span>
-          </div>
-      </div>
-      <div class="slider-card">
-          <div class="slider-image">
-              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
-          </div>
-          <div class="slider-info">
-              <h2 class="slider-title">brandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrand</h2>
-          </div>
-      </div>
-      <div class="slider-card">
-          <div class="slider-image">
-              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
-          </div>
-          <div class="slider-info">
-              <h2 class="slider-title">brandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrand</h2>
-          </div>
-      </div>
-      <div class="slider-card">
-          <div class="slider-image">
-              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
-          </div>
-          <div class="slider-info">
-              <h2 class="slider-title">brandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrandbrand</h2>
-          </div>
-      </div>
-      <div class="slider-card">
-          <div class="slider-image">
-              <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
-          </div>
-          <div class="slider-info">
-              <h2 class="slider-title">brand</h2>
-          </div>
-      </div>
-  </div>
+<section class="custom-slider">
+    <button class="pre-btn"><img src="{{ URL::asset('assets/icons/prev.svg') }}" alt=""></button>
+    <button class="nxt-btn"><img src="{{ URL::asset('assets/icons/next.svg') }}" alt=""></button>
+    <div class="slider-container">
+        <div class="slider-card">
+            <div class="slider-image">
+                <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+            </div>
+            <div class="slider-info">
+                <span class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah</span>
+            </div>
+        </div>
+        <div class="slider-card">
+            <div class="slider-image">
+                <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+            </div>
+            <div class="slider-info">
+                <span class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah</span>
+            </div>
+        </div>
+        <div class="slider-card">
+            <div class="slider-image">
+                <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+            </div>
+            <div class="slider-info">
+                <span class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah</span>
+            </div>
+        </div>
+        <div class="slider-card">
+            <div class="slider-image">
+                <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+            </div>
+            <div class="slider-info">
+                <span class="slider-title">Ibu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindah</span>
+            </div>
+        </div>
+        <div class="slider-card">
+            <div class="slider-image">
+                <img src="{{ URL::asset('assets/images/example.webp') }}" class="slider-thumb" alt="">
+            </div>
+            <div class="slider-info">
+                <span class="slider-title">asIbu Kota Baru, Polri Rapatkan Satuan Kerja yang Bakal Pindahd</span>
+            </div>
+        </div>
+    </div>
 </section>
-
